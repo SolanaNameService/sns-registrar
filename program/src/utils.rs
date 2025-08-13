a6697adf4dea59730d69e3ecc7a62fe47ddbb2b4
ellttben <elliott.benisty@gmail.com>
2025-08-14 00:55:26 +0100

    Deactivate 10+ character discount

--- program/src/utils.rs
+++ program/src/utils.rs
@@ -26,8 +26,7 @@ pub fn get_usd_price(len: usize) -> u64 {
         2 => 700,
         3 => 640,
         4 => 160,
-        i if i < 10 => 20,
-        _ => 1,
+        _ => 20,
     };
     #[cfg(not(feature = "devnet"))]
     return multiplier * 1_000_000;
